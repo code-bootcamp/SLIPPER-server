5cf0f21d58aaf17e7b23a62c2918f2001cf10879
sunghwancode <sunghwancode@gmail.com>
2022-05-24 17:44:38 +0900

    cors error

---
+++ slipper_backend/src/apis/Comment/comment.module.ts
@@ -0,0 +1,10 @@
+// import { Module } from '@nestjs/common';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { Join } from '../join/entities/join.entity';
+
+// @Module({
+//   imports: [TypeOrmModule.forFeature([Payment, Join])],
+//   providers: [PaymentResolver, PaymentService],
+// })
+// @Module({})
+// export class PaymentModule {}
