d8ee8a56bf7f9b4aaae0f12e37f35b208b908eab
parcPrive <gusahr119@gmail.com>
2022-05-28 14:21:00 +0900

    feature-#88-admin-api

--- slipper_backend/src/apis/admin/admin.resiolver.ts
+++ slipper_backend/src/apis/admin/admin.resiolver.ts
@@ -4,20 +4,48 @@ import { GqlAuthAccessGuard } from 'src/commons/auth/gql-auth.guard';
 import { RolesGuard } from 'src/commons/auth/gql-role.guard';
 import { Roles } from 'src/commons/auth/gql-role.param';
 import { BoardService } from '../Board/board.service';
+import { BusinessUserService } from '../businessBoard/businessBoard.service';
+import { CommentService } from '../Comment/comment.service';
 import { Role } from '../join/entities/join.entity';
+import { SubComment } from '../SubComment/subcomment.entity';
+import { SubCommentService } from '../SubComment/subcomment.service';
 
 @Resolver()
 export class AdminResolver {
   constructor(
     private readonly boardService: BoardService, //
+    private readonly commentService: CommentService,
+    private readonly subCommentService: SubCommentService,
+    private readonly businessUser: BusinessUserService,
   ) {}
 
   //@Roles(Role.ADMIN)
-  @UseGuards(GqlAuthAccessGuard)
   //@UseGuards(GqlAuthAccessGuard, RolesGuard)
+  // @UseGuards(GqlAuthAccessGuard)
   async deleteAdminUserBoard(
     @Args('boardId') boardId: string, //
   ) {
     return this.boardService.findOne({ boardId });
   }
+  //@Roles(Role.ADMIN)
+  //@UseGuards(GqlAuthAccessGuard, RolesGuard)
+  async deleteAdiminBusinessBoard(
+    @Args('businessBoardId') businessBoardId: string,
+  ) {
+    return this.businessUser.delete({ businessBoardId });
+  }
+
+  //@Roles(Role.ADMIN)
+  //@UseGuards(GqlAuthAccessGuard, RolesGuard)
+  //   async deleteAdminUserComment(
+  //     @Args('commentId') commentId: string, //
+  //   ) {
+  //     return this.commentService.delete({ commentId });
+  //   }
+
+  //@Roles(Role.ADMIN)
+  //@UseGuards(GqlAuthAccessGuard, RolesGuard)
+  //   async deleteAdminUserSubComment(@Args('subCommentId') SubCommentId: string) {
+  //     return this.subCommentService.delete({ sunCommentId });
+  //   }
 }
